cc1d15649f174dfd7dfdfd836f8e632c9ae420a1
samuelonime <130225015+samuelonime@users.noreply.github.com>
2026-04-14 16:29:15 +0100

    Format vite.config.js and update server settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: true,
-    port:5000,
+    port: 5000,
+
     proxy: {
       '/api': {
         target: 'https://petlove-production-53ae.up.railway.app',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
+
   build: {
     outDir: 'dist',
     sourcemap: true,
